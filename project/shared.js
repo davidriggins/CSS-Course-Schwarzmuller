ecf5648670917636923706abd7f7235e779b3677
David Riggins <driggins@esri.com>
2023-01-10 09:19:50 -0800

    Lesson 135 End. Adding Event Listeners.

--- project/shared.js
+++ project/shared.js
@@ -1,2 +1,12 @@
 var backdrop = document.querySelector(".backdrop");
-backdrop.style.display = "block";
+var modal = document.querySelector(".modal");
+
+// This will give a list of each button in the plans.
+var selectPlanButtons = document.querySelectorAll(".plan button");
+
+for (var i = 0; i < selectPlanButtons.length; i++) {
+    selectPlanButtons[i].addEventListener("click", function () {
+        modal.style.display = "block";
+        backdrop.style.display = "block";
+    });
+}
